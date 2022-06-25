e08a6b8fec2b5b2f596b051478c7cc8c82fa4cfa
berkc <b.cal.2193@gmail.com>
2022-06-25 23:02:16 +0200

    25.06 JDBC

---
+++ src/test/java/day01/Query09.java
@@ -0,0 +1,37 @@
+package day01;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Query09 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch59?serverTimezone=UTC", "root", "1234");
+        Statement st = con.createStatement();
+
+        //SORU: urunler tablosuna asagidaki verileri toplu bir sekilde ekleyin
+        List<Urun> kayitlar = new ArrayList<>();
+        kayitlar.add(new Urun(200, "Asus", 7500));
+        kayitlar.add(new Urun(201, "HP", 8500));
+        kayitlar.add(new Urun(202, "Acer", 9500));
+        kayitlar.add(new Urun(203, "Monster", 11500));
+        kayitlar.add(new Urun(204, "Klavye", 1200));
+        kayitlar.add(new Urun(205, "Fare", 1000));
+
+        PreparedStatement tablo = con.prepareStatement("insert into urunler values(?, ?, ?)");
+        for (Urun each : kayitlar) {
+        tablo.setInt(1,each.getId());
+        tablo.setString(2,each.getIsim());
+        tablo.setDouble(3,each.getFiyat());
+        tablo.addBatch();
+        }
+
+        tablo.executeBatch();
+        System.out.println("Veriler Database'e eklendi");
+        con.close();
+        st.close();
+        tablo.close();
+
+    }
+}
